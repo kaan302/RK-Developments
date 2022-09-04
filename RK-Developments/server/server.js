6365c8b023440eb09f60747677dd915e8f754e4e
Kaan <kaan.ugur@hva.nl>
2022-09-04 18:31:11 +0200

    lukt nie

--- RK-Developments/server/server.js
+++ RK-Developments/server/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const url = "mongodb+srv://RKDevelopments:Hooggeleid2022@rkdevelopments.ymjfajd.mongodb.net/?retryWrites=true&w=majority"
 
@@ -14,6 +17,25 @@ async function connect() {
 }
 
 connect();
+
+const feedbackScheme = {
+    feedback: String
+}
+
+const Raiting = mongoose.model("Raiting", feedbackScheme);
+
+// app.get("/", function (req, res){
+//     res.sendFile(__dirname + "/pricing.html")
+// })
+
+
+app.post('/post-feedback', function (req, res) {
+    let newFeedback = new Raiting({
+        feedback: req.body.feedback
+    })
+    newFeedback.save();
+    res.redirect('/pricing.html')
+});
 
 app.listen(8000, () => {
     console.log("Server started on port 8000")
